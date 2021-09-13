a7f83ac8c8ed0ae6a323ef9131fc2a471eac652d
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-13 18:36:42 -0500

    Add Prime NG module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// PrimeNG
-import { ButtonModule } from 'primeng/button';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonModule],
+  imports: [BrowserModule, PrimeNgModule],
   providers: [],
   bootstrap: [AppComponent],
 })
