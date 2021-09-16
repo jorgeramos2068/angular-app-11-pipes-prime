f03a11345c0554a64124680ade9f97bd02f3bf15
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-16 13:29:32 -0500

    Add examples for pipes that work with numbers

--- src/app/sales/pages/numbers/numbers.component.ts
+++ src/app/sales/pages/numbers/numbers.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numbers',
   templateUrl: './numbers.component.html',
 })
-export class NumbersComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class NumbersComponent {
+  public totalSales: number = 2589687.2456;
+  public percentage: number = 0.4856;
 }
