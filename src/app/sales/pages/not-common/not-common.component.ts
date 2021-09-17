6ac9e9238bb4bc454bcc2e273e0897eeef3ba080
Jorge Ramos <jose.ramos@nextiva.com>
2021-09-17 14:57:26 -0500

    Add example with i18nPluralPipe

--- src/app/sales/pages/not-common/not-common.component.ts
+++ src/app/sales/pages/not-common/not-common.component.ts
@@ -5,10 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './not-common.component.html',
 })
 export class NotCommonComponent {
+  // i18nSelect
   public name: string = 'Catwoman';
   public gender: string = 'female';
   public invitationMap = {
     male: 'him',
     female: 'her',
+  };
+
+  // i18nPlural
+  clients: string[] = ['Mary', 'Diana', 'John'];
+  clientsMap = {
+    '=0': 'no clients',
+    '=1': 'one client',
+    other: '# clients',
   };
 }
